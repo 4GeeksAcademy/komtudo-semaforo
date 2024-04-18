366746dd2918f890374d4c002dc204434531f875
KomTudo <129621963+KomTudo@users.noreply.github.com>
2024-04-18 16:07:56 +0000

    Ver.2

---
+++ src/js/component/semaforo.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Semaforo = () => {
+
+    const [selectedColor, setSelectedColor] = useState("green");
+
+    let str = ""
+    if (selectedColor === "red") str = "glow";
+
+    return (
+
+        <div className="traffic-light p-2">
+
+            <div
+                onClick={() => setSelectedColor("red")}
+                className={
+                    "light red mb-2" + (selectedColor === "red" ? "glow" : "")
+                } ></div>
+            <div 
+                onClick={() => setSelectedColor("yellow")}
+                className={
+                    "light yellow mb-2" + (selectedColor === "yellow" ? "glow" : "")
+                }></div>
+            <div 
+                onClick={() => setSelectedColor("green")}
+                className={
+                    "light green mb-2" + (selectedColor === "green" ? "glow" : "")
+                }></div>
+
+        </div>
+
+    );
+}
+
+export default Semaforo;

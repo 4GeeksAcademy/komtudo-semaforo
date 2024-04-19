dabcf6d56331256d281e84d9f53cb4565fa292f5
KomTudo <129621963+KomTudo@users.noreply.github.com>
2024-04-19 10:01:40 +0000

    Se centra el palo del semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,11 +6,18 @@ import Semaforo from "./semaforo";
 const Home = () => {
 
 	return (
-		<div className="semaforo">
-			
-			<Semaforo />
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="paloSemaforo ">
+				</div>
+			</div>
+			<div className="semaforo d-flex justify-content-center">
 
+				<Semaforo />
+
+			</div>
 		</div>
+
 	);
 };
 
